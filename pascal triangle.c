1bd29f8ad7b9c64a822d354035db0a747feeb486
sakshisinghal2911 <sakshisinghal2911@gmail.com>
2026-06-27 10:02:09 +0530

    Add files via upload

---
+++ pascal triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int rows, i, j, num;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 0; i < rows; i++) {
+        num = 1;
+        for (j = 0; j < rows - i; j++) {
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++) {
+            printf("%d ", num);
+            num = num * (i - j) / (j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
